99a1edade567fd930555953b91162389c5205644
yukimura1227 <takamura1227@gmail.com>
2017-06-04 00:29:57 +0900

    add assistant function for add column case

--- lib/migration_command_assistant.rb
+++ lib/migration_command_assistant.rb
@@ -1,5 +1,40 @@
-require "migration_command_assistant/version"
+# frozen_string_literal: true
+require 'migration_command_assistant/version'
+require 'active_support'
+require 'active_support/core_ext'
 
+# migration command assistant
 module MigrationCommandAssistant
-  # Your code goes here...
+  def self.main
+    puts 'what you want?'
+    loop do
+      puts '[1] add column [q] quit'
+      case gets.chomp
+      when '1'
+        add_column
+        exit 0
+      when 'q'
+        exit 0
+      end
+    end
+  end
+
+  def self.add_column
+    puts 'table name?'
+    table_name = gets.chomp
+    puts 'column name?'
+    column_name = gets.chomp
+    puts 'column type?(string text integer float decimal ' \
+      'datetime timestamp time date binary boolean)'
+    column_type = gets.chomp
+
+    put_line
+    puts 'rails generate migration ' \
+      "Add#{ column_name.classify }To#{ table_name.classify } #{ column_name }:#{ column_type }"
+    put_line
+  end
+
+  def self.put_line
+    puts '=' * 100
+  end
 end
