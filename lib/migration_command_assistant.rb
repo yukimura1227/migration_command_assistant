38bf47ab3f34d0b846c69dd07368a0a243a36189
yukimura1227 <takamura1227@gmail.com>
2017-06-03 23:31:40 +0900

    first commit (create by [bundle gem migration_command_assistant -t -b]

---
+++ lib/migration_command_assistant.rb
@@ -0,0 +1,5 @@
+require "migration_command_assistant/version"
+
+module MigrationCommandAssistant
+  # Your code goes here...
+end
---
+++ lib/migration_command_assistant/version.rb
@@ -0,0 +1,3 @@
+module MigrationCommandAssistant
+  VERSION = "0.1.0"
+end
